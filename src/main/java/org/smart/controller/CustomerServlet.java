0b6a255be85c6c6d9107781751c2b0a47bfa83d3
OrangeCandies <liuhui.mail@foxmail.com>
2018-01-22 17:50:28 +0800

    完善了控制层 并且使整个Demo可以跑起来了 准备自己开始写控制器

--- src/main/java/org/smart/controller/CustomerServlet.java
+++ src/main/java/org/smart/controller/CustomerServlet.java
@@ -1,19 +1,32 @@
 package org.smart.controller;
 
+import org.smart.model.Customer;
+import org.smart.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "CustomerServlet")
+@WebServlet(name = "CustomerServlet",urlPatterns = {"/customer"})
 public class CustomerServlet extends HttpServlet {
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        List<Customer> customerList = customerService.getCustomerList();
+        request.setAttribute("customerLists",customerList);
+        request.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(request,response);
     }
 }
